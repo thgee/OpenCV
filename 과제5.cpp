d4e4acf68bb8cb662217a8385ee36aeff59dfb8c
thgee <xogur25@naver.com>
2022-06-08 19:23:12 +0900

    [ADD] 이미지 경로 입력받는 기능 구현 및 코드 정리

--- 과제5.cpp
+++ 과제5.cpp
@@ -1,26 +1,16 @@
 #include <opencv2/opencv.hpp>
+#include <iostream>
 
-// 1. shift와 일반드래그 연결만 해주면 된다.
-//		---> 변수누적을 풀고 전역행렬로 이어붙인다
-//		---> drag 1, 2에서 모든 변수를 포함시킨다
-// *** 모든 누적데이터는 변수에 들어있고 행렬은 껍데기일 뿐이다.
-
-// 변수를 전역으로 빼고 모든 함수에서 행렬을 전부 적용한다
-
-// 원래 찍었던 P 기준으로 변환하고 그 결과사진을 새롭게 찍은 P 기준으로 다시 변환해야 하는데
-// 이렇게 변수에 모든 데이터를 기억하도록 하면 
-// 마지막에 찍힌 P 기준으로 단 한번의 변환에 모든게 섞이게 된다.
-
-// 그래서 R버튼 클릭 시에 src을 dst로 갱신해주면
-// 원본사진이 잘리거나 디지털 풍화가 일어난다.
-
-// 따라서 R버튼 클릭시에 현재 회전축 까지의 변환을 기억하는 행렬을 만들어서 곱해주면 어떨까?
+char path[100]; // 이미지의 경로를 입력받을 변수
+IplImage* src;
+IplImage* dst;
+IplImage* buf;
 
 // 행렬 복사 함수
-void copyMatrix(float IM[][3], float copy_IM[][3]) {
+void copyMatrix(float M[][3], float copy_M[][3]) {
 	for (int i = 0; i < 3; i++)
 		for (int j = 0; j < 3; j++)
-			copy_IM[i][j] = IM[i][j];
+			copy_M[i][j] = M[i][j];
 }
 
 // 기본행렬 초기화 함수
@@ -37,14 +27,13 @@ void setScaleMatrix(float M[][3], float sx, float sy) {
 	M[1][0] = 0;		M[1][1] = sy; // y축으로 sy만큼 곱함
 }
 
-
 // 회전 함수
-void setRotateMatrix(float M[][3], float theta) {
+void setRotateMatrix(float M[][3], float theta) { // theta는 atan2에서 나온 결과값으로 radian 값이므로 바로 적용 가능
 	setIdentityMatrix(M);
-	float rad = theta * 3.141592 / 180.0f; // degree를 radian으로 변환
 
-	M[0][0] = cos(rad);			M[0][1] = -sin(rad); // 회전행렬
-	M[1][0] = sin(rad);			M[1][1] = cos(rad);
+
+	M[0][0] = cos(theta);			M[0][1] = -sin(theta); // 회전행렬
+	M[1][0] = sin(theta);			M[1][1] = cos(theta);
 }
 
 // 위치 이동 함수
@@ -55,17 +44,17 @@ void setTranslateMatrix(float M[][3], float tx, float ty) {
 }
 
 // 3X3 행렬의 곱을 만들어주는 함수
+// 이 함수 사용시 주의할점 : setMultiplyMatrix(M1, M1, M2) 이런식으로 같은 행렬을 두번 넣어주면 안된다
+// 왜냐하면 인자로 배열의 주소값이 들어가기 때문에 +=로 중첩되는 과정에서 증가한 배열이 다시 함수에 들어가기 때문에 정상적인 연산이 안된다
 void setMultiplyMatrix(float M[][3], float A[][3], float B[][3]) {
 	// M = A*B
 	for (int i = 0; i < 3; i++)
 		for (int j = 0; j < 3; j++) {
-			M[i][j] = 0; // 0으로 초기화
+			M[i][j] = 0; // 행렬을 0으로 초기화
 			for (int k = 0; k < 3; k++)
 				M[i][j] += A[i][k] * B[k][j];
 		}
 }
-
-
 
 // 변환함수 M을 src에 적용하여 변형된 사진인 dst을 만드는 함수
 void applyAffineTransform(IplImage* src, IplImage* dst, float M[][3]) {
@@ -87,35 +76,30 @@ void applyAffineTransform(IplImage* src, IplImage* dst, float M[][3]) {
 }
 
 
+// 원점이동TM1 -> 확대축소SM -> 회전RM -> 사진이동TM -> 원위치이동TM2
+float cx; // 이미지의 중심좌표
+float cy;
+CvPoint P; // 피봇좌표
 
-IplImage* src = cvLoadImage("c:\\temp\\lena.jpg");
-IplImage* dst = cvCreateImage(cvGetSize(src), 8, 3);
-IplImage* buf = cvCreateImage(cvGetSize(src), 8, 3);
-float cx = dst->width / 2.0f;
-float cy = dst->height / 2.0f;
-CvPoint P = cvPoint(cx, cy);
+float IM[3][3]; // 최종 변환행렬
+float copy_IM[3][3]; // 행렬IM을 복사한 행렬
 
-float IM[3][3];
-float copy_IM[3][3];
-
-float theta = 0.0f;
-float scale = 1.0f;
-float tx = 0.f;
+float theta = 0.0f; // 회전각도 (degree)
+float scale = 1.0f; // 확대축소 비율
+float tx = 0.f; // 이미지 이동 정도
 float ty = 0.f;
 
-// 원점이동TM1 -> 확대축소M2 -> 회전M1 -> 사진이동TM -> 원위치이동TM2
-
-float M1[3][3];
-float M2[3][3];
-float TM[3][3];
-float TM1[3][3];
-float TM2[3][3];
-float temp1[3][3];
+float RM[3][3]; // 회전행렬
+float SM[3][3]; // 확대축소행렬
+float TM[3][3]; // 이동행렬
+float TM1[3][3]; // 피봇 ->원점 이동행렬
+float TM2[3][3]; // 원점 -> 피봇 이동행렬
+float temp1[3][3]; // 행렬을 곱할 때 사용되는 temp행렬들
 float temp2[3][3];
 float temp3[3][3];
 float temp4[3][3];
 
-float rem[3][3]; // 이전 회전축에서의 변환을 기억하는 행렬
+float rem[3][3]; // 이전 피봇에서의 변환을 기억하는 행렬
 
 CvPoint pt1; // L버튼을 클릭한 지점을 기억하고 있는 전역변수
 int draging = 0; // 그냥 드래그 하면 1, shift + 클릭 후 드래그 하면 2로 설정 
@@ -134,18 +118,19 @@ void myMouse(int event, int x, int y, int flags, void* param) {
 		draging = 1;
 	}
 	if (event == CV_EVENT_RBUTTONDOWN) {
-		P = cvPoint(x, y);
-		
+		P = cvPoint(x, y); // 오른쪽버튼 클릭 시 피봇좌표를 저장
+
 
 		theta = 0.0f; // 변수값들 초기화
-		scale = 1.0f;
+		scale = 1.0f; // 초기화를 해도 되는 이유는 이미 rem행렬에 이전까지의 변환행렬들의 곱을 저장했기 때문
 		tx = 0.f;
 		ty = 0.f;
 
 
 		copyMatrix(IM, rem); // IM을 rem에 저장
 
-		cvCopy(dst, buf);
+		cvCopy(dst, buf); // buf이미지에 피봇지점을 찍고 이미지를 출력함으로써 dst이미지는 깨끗한 상태를 유지한다
+						  // buf를 이용하지 않고 dst에 직접 피봇을 찍으면 피봇을 재설정 할 때마다 이전의 점이 지워지지 않고 계속 늘어난다
 		cvCircle(buf, P, 5, cvScalar(255, 0, 0), -1);
 		cvShowImage("dst", buf);
 	}
@@ -154,60 +139,53 @@ void myMouse(int event, int x, int y, int flags, void* param) {
 		if (draging == 1) { // 일반 드래그 일 때
 			CvPoint pt2 = cvPoint(x, y); // 마우스를 드래그할 때의 좌표를 pt2에 저장
 			// 회전
-			float theta1 = atan2(pt1.y - P.y, pt1.x - P.x); // atan을 이용하면 좌표에 따른 각도가 결과값으로 나옴
-			float theta2 = atan2(pt2.y - P.y, pt2.x - P.x); // atan2인 이유는 tan범위 때문에 opencv에서 편의상 만든 함수임
-			// 각도는 degree로 넣어야 하는데 atan2에서 나오는 값은 radian이라 변환이 필요
-			theta += (theta2 - theta1) / 3.141592 * 180.0f; // 각도가 계속 누적이 되어야 현재 그림에서 각도 변경이 가능하다
+			float theta1 = atan2(pt1.y - P.y, pt1.x - P.x); // 피봇좌표와 현재 클릭한 좌표의 x차이, y차이를 atan2함수에 넣어서 그 각도를 얻어온다
+			float theta2 = atan2(pt2.y - P.y, pt2.x - P.x); // atan2는 atan의 tan범위를 고려하여 opencv에서 편의상 만든 함수임
+			theta += (theta2 - theta1);// 각도가 계속 누적이 되어야 src로부터 회전한 각도를 theta가 기억하여 연속적인 변환이 가능하다
 
 			// 확대축소
-			float r1 = sqrt((pt1.x - P.x) * (pt1.x - P.x) + (pt1.y - P.y) * (pt1.y - P.y)); // pt1과 회전축 사이의 거리
-			float r2 = sqrt((pt2.x - P.x) * (pt2.x - P.x) + (pt2.y - P.y) * (pt2.y - P.y)); // pt2와 회전축 사이의 거리
+			float r1 = sqrt((pt1.x - P.x) * (pt1.x - P.x) + (pt1.y - P.y) * (pt1.y - P.y)); // pt1과 피봇 사이의 거리
+			float r2 = sqrt((pt2.x - P.x) * (pt2.x - P.x) + (pt2.y - P.y) * (pt2.y - P.y)); // pt2와 피봇 사이의 거리
 			scale *= (r2 / r1);
 
-			
-
-			// TM1은 그림의 중심을 원점으로 이동시키는 행렬
-			// TM2는 다시 그림을 원위치로 이동시키는 행렬
-
-
 			pt1 = pt2; // 이걸 안해주면 드래그 시에 pt1이 처음상태에 고정되고 pt2는 누적적으로 커지므로 그림이 확확 커진다.
-					   // pt1을 현재 위치인 pt2로 초기화를 해주어야 한다.
-
+					   // pt1을 현재 위치인 pt2로 초기화를 해줌으로써 딱 마우스를 이동하는 만큼씩만 누적되도록 한다
 		}
 		if (draging == 2) { // shift + L버튼 클릭 후 드래그 일 때
-			// 여기서 중요한것은 사진의 위치가 변경될 때 회전축(P) 좌표를 함께 변동시켜 주어야 한다는 점이다.
 
 			CvPoint pt2 = cvPoint(x, y); // 마우스를 드래그할 때의 좌표를 pt2에 저장
 
-			tx += (pt2.x - pt1.x); // 마우스를 클릭한 위치부터 d떼는 위치까지의 x, y좌표 변화량만큼 사진이 이동해야 한다.
+			tx += (pt2.x - pt1.x); // 마우스를 클릭한 위치부터 떼는 위치까지의 x, y좌표 변화량만큼 사진이 이동해야 한다.
 			ty += (pt2.y - pt1.y);
-			P.x += (pt2.x - pt1.x); // 회전축도 함께 이동해야 한다
+			P.x += (pt2.x - pt1.x); // 피봇도 함께 이동해야 한다
 			P.y += (pt2.y - pt1.y);
 
 			pt1 = pt2;
 		}
 
+		// TM1은 피봇좌표를 원점으로 이동시키는 행렬
+		// TM2는 피봇좌표를 다시 원위치하는 행렬
 
-		setRotateMatrix(M1, -theta); // 사진을 회전하는 IM행렬 생성
-		setScaleMatrix(M2, 1.0f / scale, 1.0f / scale);
+		setRotateMatrix(RM, -theta); // 사진을 회전하는 IM행렬 생성
+		setScaleMatrix(SM, 1.0f / scale, 1.0f / scale);
 		setTranslateMatrix(TM1, P.x, P.y);
 		setTranslateMatrix(TM2, -P.x, -P.y);
 		setTranslateMatrix(TM, -tx, -ty);
 
-	/*	 M = TM2 M1 M2 TM TM1 rem
-		 IM = rem TM1 TM M2 M1 TM2  */
+		// 행렬 곱하는 순서
+		//	 M = TM2 RM SM TM TM1 rem 정방향
+		//	 IM = rem TM1 TM SM RM TM2 역방향
 
 		setMultiplyMatrix(temp1, rem, TM1);
-		setMultiplyMatrix(temp2, TM, M2);
-		setMultiplyMatrix(temp3, M1, TM2);
+		setMultiplyMatrix(temp2, TM, SM);
+		setMultiplyMatrix(temp3, RM, TM2);
 		setMultiplyMatrix(temp4, temp1, temp2);
 		setMultiplyMatrix(IM, temp4, temp3);
-		
 
 		applyAffineTransform(src, dst, IM); // 변환행렬 적용
 
 		cvCopy(dst, buf);
-		cvCircle(buf, P, 5, cvScalar(255, 0, 0), -1); // 회전축을 그려줌
+		cvCircle(buf, P, 5, cvScalar(255, 0, 0), -1); // 피봇을 그려줌
 
 		cvShowImage("dst", buf);
 	}
@@ -216,19 +194,34 @@ void myMouse(int event, int x, int y, int flags, void* param) {
 
 int main() {
 
+	std::cout << "Left Mouse Dragging : Rotating / Scaling" << std::endl;
+	std::cout << "Left Mouse Dragging + Shiftkey : Translating" << std::endl;
+	std::cout << "Right Mouse Click : Resetting the pivot point" << std::endl;
+	std::cout << "=============================================" << std::endl;
+	std::cout << "InputFile Path : ";
+	std::cin >> path;
+	src = cvLoadImage(path);
+	dst = cvCreateImage(cvGetSize(src), 8, 3);
+	buf = cvCreateImage(cvGetSize(src), 8, 3);
+
+	cx = dst->width / 2.0f;
+	cy = dst->height / 2.0f;
+	P = cvPoint(cx, cy); // 피봇의 초기값을 이미지의 중심으로 설정
+
 	cvCopy(src, dst);
 
+	// 변환행렬들을 기본행렬로 초기화
 	// 초깃값을 기본행렬로 안해주면 shift나 Lbutton 을 맨 처음 클릭 시에 multiplyMatrix에 이상한 행렬이 들어가서 변환이 비정상적으로 된다.
 	setIdentityMatrix(IM);
 	setIdentityMatrix(rem);
-	setIdentityMatrix (M1);
-	setIdentityMatrix (M2);
-	setIdentityMatrix (TM);
-	setIdentityMatrix (TM1);
-	setIdentityMatrix (TM2);
+	setIdentityMatrix(RM);
+	setIdentityMatrix(SM);
+	setIdentityMatrix(TM);
+	setIdentityMatrix(TM1);
+	setIdentityMatrix(TM2);
 
-	cvShowImage("dst", dst);
-	cvSetMouseCallback("dst", myMouse); // dst이미지에서 myMouse함수 적용 (암기) 
+	cvShowImage("dst", dst); // 처음 화면엔 피봇이 그려지지 않은 이미지가 출력
+	cvSetMouseCallback("dst", myMouse); // dst이미지에서 myMouse함수 적용
 	cvWaitKey();
 
 	return 0;
